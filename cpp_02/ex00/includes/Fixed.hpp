9a2aae2c9f2df42dbd9e823de1955ceb838fb362
Guiribei <guilherme_f_ribeiro@hotmail.com>
2023-05-14 18:18:06 -0300

    feat: start ex00 cpp_02

---
+++ cpp_02/ex00/includes/Fixed.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/14 18:13:44 by guribeir          #+#    #+#             */
+/*   Updated: 2023/05/14 18:16:46 by guribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class	Fixed
+{
+	private:
+	
+	public:
+
+	Fixed( void );
+	~Fixed( void );
+};
+
+#endif
