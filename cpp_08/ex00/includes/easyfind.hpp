e85c682f52b458f5c9b76ae9b1dba25114feecb1
Guiribei <guilherme_f_ribeiro@hotmail.com>
2023-07-10 22:15:26 -0300

    feat(cpp_08): started

---
+++ cpp_08/ex00/includes/easyfind.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/10 22:12:23 by guribeir          #+#    #+#             */
+/*   Updated: 2023/07/10 22:14:22 by guribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <iostream>
+
+template <typename T>
+int easyfind(T &container, int n)
+{
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw std::exception();
+	return *it;
+}
+
+#endif
